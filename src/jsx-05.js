9c9e1cabad090d78f995b4f81e39371411032f8c
EnzoSt <Stellavatoe@gmail.com>
2022-05-11 09:02:28 +0200

    jsx-05

---
+++ src/jsx-05.js
@@ -0,0 +1,7 @@
+//Write a function that receives two parameters, `a` and `b` and returns the sum of them within an `h2` tag.
+
+function sum(a, b) {
+  return <h2>The sum is {a + b}</h2>;
+}
+
+sum(2, 3);
